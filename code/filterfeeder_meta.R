9801663e675e9d9311e8a011d5fe063d01da5916
whippor <ross.whippo@noaa.gov>
2024-06-26 12:26:39 -0800

    Updated literature sheet, added additional analyses

--- code/filterfeeder_meta.R
+++ code/filterfeeder_meta.R
@@ -51,11 +51,17 @@ str(lit_filtration)
 unique(lit_filtration$unit)
 
 lit_combined <- lit_filtration %>% 
-  mutate(rate = coalesce(rate_formula, `calculated g rate`))
+  mutate(rate = coalesce(rate_formula, `calculated g rate`)) %>%
+  mutate(species = factor(species, levels = c("Crassostrea virginica",
+                                "Magallana gigas",
+                                "Mytilus edulis",
+                                "Mytilus trossulus",
+                                "Mytilus californianus",
+                                "Mytilus galloprovinciallis")))
 
 # mean per individuals
 indiv <- lit_combined %>%
-  filter(unit %in% c("l_hr_ind", "ml_min_ind", "ml_hr_ind")) %>%
+  filter(unit %in% c("l_hr_ind", "ml_min_ind", "ml_hr_ind", "l_h_ind")) %>%
   filter(min_max_mean == "mean")
 
 indv_stand <- indiv %>%
@@ -64,13 +70,22 @@ indv_stand <- indiv %>%
                                          .default = rate_formula)) %>%
   filter(!is.na(standard_filtration))
 
-indv_stand %>%
-  ggplot(aes(x = species, y = standard_filtration, fill = min_max_mean)) +
+# number of studies
+unique(indv_stand$source)
+
+indv_gg <- indv_stand %>%
+  ggplot(aes(x = species, y = standard_filtration, fill = species)) +
   geom_boxplot(alpha = 0.3, width = 0.2, linewidth = 1.05) +
   geom_jitter(shape = 21, size = 5, width = 0.1, alpha = 0.7) +
-  scale_fill_viridis(discrete = TRUE, begin = 0.2) +
+  scale_x_discrete(drop = FALSE) +
+  scale_fill_viridis(discrete = TRUE, begin = 0.2, drop = FALSE) +
   ylab(expression(paste(L, ~h^{-1} , ~ind^{-1}))) +
-  theme_bw()
+  theme_bw() + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0, 0.5, 0, 0.5), "cm")) +
+  annotate("text", x = 5.5, y = 3.75, label = "studies (n) = 3")
 
 # mean per unit weight
 dryweight <- lit_combined %>%
@@ -81,13 +96,22 @@ weight_stand <- dryweight %>%
   mutate(standard_filtration = case_when(unit == "ml_hr_g" ~ rate * 0.001,
                                          .default = rate))
 
-weight_stand %>%
-  ggplot(aes(x = species, y = standard_filtration, fill = min_max_mean)) +
+# number of studies
+unique(weight_stand$source)
+
+weight_gg <- weight_stand %>%
+  ggplot(aes(x = species, y = standard_filtration, fill = species)) +
   geom_boxplot(alpha = 0.3, width = 0.2, linewidth = 1.05) +
   geom_jitter(shape = 21, size = 5, width = 0.1, alpha = 0.7) +
-  scale_fill_viridis(discrete = TRUE, begin = 0.5) +
+  scale_x_discrete(drop = FALSE) +
+  scale_fill_viridis(discrete = TRUE, begin = 0.2, drop = FALSE) +
   ylab(expression(paste(L, ~g^{-1} , ~h^{-1}))) +
-  theme_bw()
+  theme_bw() + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0, 0.5, 0, 0.23), "cm")) +
+  annotate("text", x = 5.5, y = 16, label = "studies (n) = 17")
 
 # z-score comparison
 zscore <- lit_combined %>%
@@ -100,12 +124,23 @@ zscore <- lit_combined %>%
   mutate(`z score` = ((coal_rate - mean(coal_rate, na.rm = TRUE))/sd(coal_rate, na.rm = TRUE))) %>%
   filter(!is.na(`z score`))
 
-zscore %>%
-  ggplot(aes(x = species, y = `z score`, fill = min_max_mean)) +
+# number of studies
+unique(zscore$source)
+
+zscore_gg <- zscore %>%
+  ggplot(aes(x = species, y = `z score`, fill = species)) +
   geom_boxplot(alpha = 0.3, width = 0.2, linewidth = 1.05) +
   geom_jitter(shape = 21, size = 5, width = 0.1, alpha = 0.7) +
-  scale_fill_viridis(discrete = TRUE, begin = 0.7) +
-  theme_bw()
+  scale_x_discrete(drop = FALSE) +
+  scale_fill_viridis(discrete = TRUE, begin = 0.2, drop = FALSE) +
+  theme_bw() + 
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0, 0.5, 0, 0.6), "cm")) +
+  annotate("text", x = 5.5, y = 4, label = "studies (n) = 19")
+
+ggarrange(indv_gg, weight_gg, zscore_gg, ncol = 1, common.legend = TRUE)
 
 
 ############### SUBSECTION HERE
